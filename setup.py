6faf44f4bb180234b372284b3fc134bfa7fedfe7
Akshat Sharma <akshatsharma5877@gmail.com>
2025-02-21 10:15:09 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='implementing piplelines, data versioning and data cloud storage (AWSs3) together.',
+    author='Akshat',
+    license='',
+)
